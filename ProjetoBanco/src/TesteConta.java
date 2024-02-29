4fe41b89358306f4db202266331db9d98400cef1
Pedro Henrique Alves <pedro.henriquerealalvessp@gmail.com>
2024-02-29 00:50:29 -0300

    Construtores

--- ProjetoBanco/src/TesteConta.java
+++ ProjetoBanco/src/TesteConta.java
@@ -2,7 +2,7 @@ public class TesteConta {
 
     public static void main(String[] args) {
 
-        Conta cc = new Conta(1000, 50.0);  // FOI GERADO O OBJETO EM MEMORIA CHAMADO contaCorrente
+        Conta cc = new Conta(1000, 60.0);  // FOI GERADO O OBJETO EM MEMORIA CHAMADO contaCorrente
 
         System.out.println(cc.recuperarSaldo());
 
